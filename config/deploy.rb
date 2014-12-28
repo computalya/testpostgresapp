b3cd276993e80c50f4c44a2ec87011bc0018e3c2
Atilla Gündüz <gunduz.atilla@gmail.com>
2014-12-28 11:47:51 +0200

    added git to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,10 @@ set :repo_url, 'git@github.com:computalya/testpostgresapp.git'
 set :deploy_to, '/opt/www/testpostgresapp'
 set :user, 'deploy'
 set :ssh_options, { :forward_agent => true }
+set :use_sudo, false
 
 # Default value for :scm is :git
-# set :scm, :git
+ set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
